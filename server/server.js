edb75eebb720700200f90bc922f7280a6d5253e5
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-10-15 17:13:57 +0800

    delete unused file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const express = require('express'),
       Schema = require('./schema.js'),
       path = require('path'),
       cors = require('cors'),
-      // routes = require('./routes.js'),
       port = 8000;
 
 const mongoose = require('mongoose');
@@ -21,23 +20,6 @@ const notFoundError = (err, req, res, next) => {
 };
 
 app.use(cors());
-// app.use('/api', routes);
-// app.get('*', function(req, res, next) {
-//   var err = new Error();
-//   err.status = 404;
-//   next(err);
-// });
-
-// app.use(notFoundError);
-// app.use((err, req, res, next) => {
-//   res.status(500).send({
-//     error: err,
-//     message: 'Internal error!'
-//   });
-// });
-
-
-
 
 app.use('/api', graphqlHTTP({
   schema: Schema,
