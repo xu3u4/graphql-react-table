fc127f315d0f6bc0f19eba0e348bd81d3cf6eeb0
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-10-22 17:49:16 +0800

    delete unused initial value

--- app/js/reducers/reducer_issues.js
+++ app/js/reducers/reducer_issues.js
@@ -1,7 +1,4 @@
 export default function (state = {
-  issues: [
-    { seq: 1, Status: 'Open', Category: 'cat1', Title: 'title1', Owner: 'Allen', Priority: '1' }
-  ],
   selectedIssue: {},
   newIssue: 0,
   isShowWarning: false
